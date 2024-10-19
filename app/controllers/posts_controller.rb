8244a2550dbc88c289c6c8e7fe585785644d70f5
mauricio-chirino <mchirino@aniracloud.com>
2024-10-19 10:16:51 -0400

    funcionan las reacciones

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,61 +1,46 @@
 class PostsController < ApplicationController
-     before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
-     def index
-       @posts = Post.all.includes(:user)
-     end
+  def index
+    @posts = Post.all.includes(:user)
+  end
 
 
      
-    def show
-      @post = Post.find(params[:id])
-      @comments = @post.comments.includes(:user)
-      @comment = Comment.new
-
-       respond_to do |format|
-         
-          format.turbo_stream { render turbo_stream: turbo_stream.replace("post-details", partial: "posts/post_details", locals: { post: @post }) }
-          format.html 
-       end
-
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments.includes(:user)
+    @comment = Comment.new
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("post-details", partial: "posts/post_details", locals: { post: @post }) }
+      format.html 
     end
+  end
    
 
+  def new
+    @post = Post.new
+  end
 
 
-     def new
-       @post = Post.new
-     end
-
-
-
-
-    def create
-        @post = current_user.posts.new(post_params)
-
-
-
-        if @post.save
-          redirect_to @post, notice: 'Post creado exitosamente.'
-        else
-          render :new
-        end
+  def create
+    @post = current_user.posts.new(post_params)
+      if @post.save
+        redirect_to @post, notice: 'Post creado exitosamente.'
+      else
+        render :new
+      end
 
 
 
-        
+
+  end
+
+
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
     end
-
-
-
-
-
-
-
-
-     private
-
-     def post_params
-       params.require(:post).permit(:title, :body)
-     end
-   end
+  end
--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,11 +1,41 @@
 class ReactionsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @reaction = @post.reactions.new(user: current_user, like: params[:like])
+
+    # Busca el usuario anónimo si no hay un usuario autenticado
+    if current_user.nil?
+      # El usuario no está autenticado (current_user es nil)
+      anonymous_user = User.find_by(email: 'anonimo@anonimo.com')
+    end
+    
+    # Usa el usuario actual si está autenticado o el usuario anónimo si no está autenticado
+    user = current_user || anonymous_user
+
+    # Log para verificar el usuario asignado (autenticado o anónimo)
+    #Rails.logger.info "Usuario asignado: #{user.inspect}"
+
+    # Crear nueva reacción asociada al usuario (autenticado o anónimo)
+    #@reaction = @post.reactions.new(user: user, like: reaction_params[:like])
+
+    # Crear o actualizar la reacción asociada al usuario (autenticado o anónimo)
+    @reaction = @post.reactions.find_or_initialize_by(user: user)
+    @reaction.like = reaction_params[:like]
+
     if @reaction.save
-      redirect_to @post, notice: 'Reacción registrada exitosamente.'
+      respond_to do |format|
+        format.turbo_stream # Responder con Turbo Stream
+        format.html { redirect_to @post, notice: 'Reacción registrada exitosamente.' }
+      end
     else
-      redirect_to @post, alert: 'Error al registrar la reacción.'
+      respond_to do |format|
+        format.html { redirect_to @post, alert: 'Error al registrar la reacción.' }
+      end
     end
   end
- end
+
+  private
+
+  def reaction_params
+    params.require(:reaction).permit(:like)
+  end
+end
