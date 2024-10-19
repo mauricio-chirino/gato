94663e3defcde2750021d62b22619be0a01248ec
mauricio-chirino <mchirino@aniracloud.com>
2024-10-19 16:38:38 -0400

    aplicacion funcionando

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -12,24 +12,16 @@ class ReactionsController < ApplicationController
     user = current_user || anonymous_user
 
     # Log para verificar el usuario asignado (autenticado o anónimo)
-    #Rails.logger.info "Usuario asignado: #{user.inspect}"
+    Rails.logger.info "Usuario asignado: #{user.inspect}"
 
     # Crear nueva reacción asociada al usuario (autenticado o anónimo)
-    #@reaction = @post.reactions.new(user: user, like: reaction_params[:like])
-
-    # Crear o actualizar la reacción asociada al usuario (autenticado o anónimo)
-    @reaction = @post.reactions.find_or_initialize_by(user: user)
-    @reaction.like = reaction_params[:like]
+    @reaction = @post.reactions.new(user: user, like: reaction_params[:like])
 
     if @reaction.save
-      respond_to do |format|
-        format.turbo_stream # Responder con Turbo Stream
-        format.html { redirect_to @post, notice: 'Reacción registrada exitosamente.' }
-      end
+      redirect_to @post, notice: 'Reacción registrada exitosamente.'
+      
     else
-      respond_to do |format|
-        format.html { redirect_to @post, alert: 'Error al registrar la reacción.' }
-      end
+      redirect_to @post, alert: 'Error al registrar la reacción.'
     end
   end
 
